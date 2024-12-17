2dd5e08ba546313247d04fa667e2d433ef86f56e
Turtlemay <am@trtl.in>
2024-12-16 21:34:06 -0500

    Pushing history entry for scroll anchor.

--- public/scripts/smooth-scroll-anchors.js
+++ public/scripts/smooth-scroll-anchors.js
@@ -1,6 +1,9 @@
 for (const v of document.querySelectorAll("a[href^='#']")) {
 	v.addEventListener("click", e => {
 		e.preventDefault();
+
+		history.pushState(null, "", v.getAttribute("href"));
+
 		document.querySelector(v.getAttribute("href"))
 			.scrollIntoView({ behavior: "smooth" });
 	});
