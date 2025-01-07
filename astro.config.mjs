e14495ce13fe71d277826a34f086fa04311d10b6
Turtlemay <am@trtl.in>
2025-01-07 10:20:20 -0500

    Using astro mdx integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
 	devToolbar: { enabled: false },
+	integrations: [
+		mdx(),
+	],
 	server: {
 		host: true,
 	},
