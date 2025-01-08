4ece0d47d45cb4bd4ac11c48162e6c3a3cb9baeb
Turtlemay <am@trtl.in>
2025-01-07 20:07:02 -0500

    Implemented blog post collection and page, drafted first post.

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+	posts: defineCollection({
+		type: "content",
+		schema: ({ image }) => z.object({
+			draft: z.boolean().optional(),
+			author: z.string(),
+			title: z.string(),
+			description: z.string(),
+			coverImage: image().optional(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			tags: z.array(z.string()).optional(),
+		}),
+	}),
+};
