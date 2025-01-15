43861c884a3d0fc754f6d3f48da8f185328fb334
Turtlemay <am@trtl.in>
2025-01-15 16:01:20 -0500

    Added preact astro integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-	devToolbar: { enabled: false },
-	integrations: [
-		mdx(),
-	],
-	server: {
-		host: true,
-	},
+    devToolbar: { enabled: false },
+    integrations: [mdx(), preact()],
+    server: {
+        host: true,
+    },
 });
