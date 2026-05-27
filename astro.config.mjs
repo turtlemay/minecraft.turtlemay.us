2c63ef23419276f6ad5956f4908fdb81a8571dea
Turtlemay <am@trtl.in>
2026-05-26 22:34:29 -0400

    Removed unused preact dependecy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import preact from "@astrojs/preact";
 import { dataFetcher } from "./api-server-data/astro-integration.ts";
 
 export default defineConfig({
     devToolbar: { enabled: false },
-    integrations: [mdx(), preact(), dataFetcher()],
+    integrations: [mdx(), dataFetcher()],
     server: {
         host: true,
     },
